9ad0f98138bb47c807edf0f3d6fced1c34c90685
leo <leo.richardo@students.amikom.ac.id>
2020-03-11 12:13:21 +0700

    menambahkan fungsi main

--- calculator.cs
+++ calculator.cs
@@ -2,8 +2,14 @@ class Calculator
 {
 	static void Main(string[] args)
 	{
+		int a = 10;
+		int b = 6;
 		
-	}
+		Console.WriteLine("Hasil Penambahan (0) + (1) = (2)", a, b, Penambahan(a,b));
+		Console.WriteLine("Hasil Pengurangan (0) - (1) = (2)", a, b, Penambahan(a,b));
+		
+		Console.WriteLine(\nTurungtungtung)
+	}	
 	
 	static int Penambahan(int a, int b)
 	{
@@ -14,4 +20,5 @@ class Calculator
 	{
 		return a - b;
 	}
+
 }
